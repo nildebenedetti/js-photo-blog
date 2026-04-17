5ea0c1e87ad6a7a699c84bcb6fda807fb73bc1d2
“Nilde <nilde.benedetti@gmail.com>
2026-04-17 13:02:06 +0200

    add: funzione return cards con test e statement of intent - script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,31 @@
 const loadingMsg = document.querySelector('.loading-content'); // div con msg caricamento
 const errorMsg = document.querySelector('.error-content');     // div con msg errore
 
+//======== MY FUNCTIONS =======//
+
+// prende come argomento un array di oggetti
+// struttura una card in html 
+// valorizza la card con i dati presenti nellárray di oggetti
+// utilizzando un for of per scorrere tutti gli indici e prendere quello che serve
+
+function returnCards(ArrayofObjects) {
+    console.log(ArrayofObjects); //test 
+    
+}
+
 
 /*1. Costruire la fetch in js e farla funzionare**
-- controlla che la richiesta venga processata correttamente con il method json()
-- setta correttamente sia catch che finally restituendo allútente update su cosa sta succedendo in pagina
 - dentro il .then dovremo arcodare una funzione per gestire la creazione delle card scorrendo lárray degli oggetti*/
 
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/'; // dichiaro variabile per API 
-// manomissione API URL per test
 
 fetch(API_URL) // inserisco la fetch - PIANO TEMPORALE PRESENTE
     .then ((result) => { // promessa di restituire dati - PIANO TEMPORALE CODICE ASINCRONO 1
         return result.json(); // quando avro'i dati, li converto con il method json e li restituisco al prossimo then
     })
     .then ((jsonData) => { // quando avro' i dati - PIANO TEMPORALE CODICE ASINCRONO 2
-        console.log(jsonData); // stampero'in console per check
+        // inizio a costruire le mie card in questo piano temporale
+        returnCards(jsonData);
     })
     .catch ((error) => { // SE si risolve in caso negativo
         console.log('error'); // check
